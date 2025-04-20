9f178942c9973e5418b85150225393680ff25995
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-04-20 10:52:36 +0545

    Update script.js
    Points Lights added

--- src/script.js
+++ src/script.js
@@ -96,3 +96,11 @@ torus.geometry.setAttribute(
 
 
 scene.add(sphere, plane, torus)
+
+ /** Lights * */ const ambientLight = new THREE.AmbientLight(0xffffff ,0.5) 
+    scene.add(ambientLight)  
+        const pointLight = new THREE.PointLight(0xffffff , 0.5) 
+        pointLight.position.x =2 
+        pointLight.position.y =3 
+        pointLight.position.z =4 
+        scene.add(pointLight)   
