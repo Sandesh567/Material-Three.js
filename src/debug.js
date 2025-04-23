6922b99fbb9aad66e7ce424026fe99ff6d526413
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-04-23 06:45:18 +0545

    Create debug.js

---
+++ src/debug.js
@@ -0,0 +1,3 @@
+import * as THREE from ’three’ import { OrbitControls } from ’three/examples/jsm/controls/OrbitControls.js’ import gsap from ’gsap’ import * as dat from ’dat.gui’     // Debug UI 
+  const gui = new dat.GUI() //parameters for color and spin when clicked 
+function const parameters ={ color : 0x6381db, spin : () => { gsap.to(mesh.rotation,{duration:1, y:mesh.rotation.y+10}) } }  gui .addColor(parameters,’color’) .onChange(()=> { material.color.set(parameters.color) })  gui .add(parameters,’spin’) 
