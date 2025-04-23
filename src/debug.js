75bbdaab0ea448196509e55cb31d503b5ca88059
Sandesh Raj Bhatta <106328473+Sandesh567@users.noreply.github.com>
2025-04-23 06:46:17 +0545

    Update debug.js
    Creating scenes and canvas

--- src/debug.js
+++ src/debug.js
@@ -1,3 +1,8 @@
 import * as THREE from ’three’ import { OrbitControls } from ’three/examples/jsm/controls/OrbitControls.js’ import gsap from ’gsap’ import * as dat from ’dat.gui’     // Debug UI 
   const gui = new dat.GUI() //parameters for color and spin when clicked 
 function const parameters ={ color : 0x6381db, spin : () => { gsap.to(mesh.rotation,{duration:1, y:mesh.rotation.y+10}) } }  gui .addColor(parameters,’color’) .onChange(()=> { material.color.set(parameters.color) })  gui .add(parameters,’spin’) 
+
+/** * Base */ // Canvas 
+const canvas = document.querySelector(’canvas.webgl’)  
+// Scene 
+const scene = new THREE.Scene() 
