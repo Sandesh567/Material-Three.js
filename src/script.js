30269c57b9c6990e84cbb87c0e2edfee6294c2e2
sandesh <sandeshbhatta218@gmail.com>
2025-04-26 12:25:02 +0545

    Updating readMe file.

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+
 /*
 
 Debug
@@ -39,6 +40,8 @@ const enviromentMapTextures = cubeTextureLoader.load(
         '/textures/environmentMaps/3/nz.jpg'
     ])
 
+
+
 /**
  * Base
  */
@@ -47,6 +50,51 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * 
+ Objects 
+ * 
+ */
+//const material = new THREE.MeshBasicMaterial()
+//material is mappedd inside using that and texture is loaded above and shown below
+// material.map = colorTexture   
+//material color is set by calling the three color function and setting it
+// material.color = new THREE.Color('green')
+// material.wireframe =true
+// material.transparent = true
+// material.opacity = 0.5
+// material.alphaMap = alphaTexture
+// material.side = THREE.DoubleSide
+
+// const material = new THREE.MeshNormalMaterial
+// material.flatShading  = true
+
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// const material = new THREE.MeshDepthMaterial()
+
+// const material = new THREE.MeshLambertMaterial()
+
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 1000
+// material.specular = new THREE.Color('red')
+
+// const material = new THREE.MeshToonMaterial()
+
+// material.map = colorTexture
+// material.aoMAp = doorambientOcclusionTexture
+// material.aoMapIntensity =  1
+// material.displacementMap = heightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = metalnessTexture
+// material.roughnessMap = roughnessTexture
+// material.normalMap = normalTexture
+// material.normalScale.set(1,1)
+// material.transparent =true
+// material.alphaMap = dooralphaTexture
+// material.wireframe =true
 
 const material = new THREE.MeshStandardMaterial()
 material.metalness = 0.7
@@ -59,7 +107,6 @@ material.envMap = enviromentMapTextures
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
 gui.add(material, 'roughness').min(0).max(1).step(0.0001)
 gui.add(material, 'displacementScale').min(0).max(1).step(0.0001)
-
 
 
 const sphere = new THREE.Mesh(
@@ -95,4 +142,98 @@ torus.geometry.setAttribute(
 
 
 
+
+
 scene.add(sphere, plane, torus)
+
+
+
+/**
+ Lights
+ * 
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    //Update Objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.y = 0.15 * elapsedTime
+    torus.rotation.y = 0.15 * elapsedTime
+
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
